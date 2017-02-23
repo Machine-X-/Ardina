1bd1d87dc5cb8ca4f604e6705d304d848a411904
Crystal Ceballos <ceballos.2@osu.edu>
2017-02-22 19:49:38 -0500

    Updated VideoActivityTest

--- MyArdina/app/src/androidTest/java/com/myardina/buckeyes/myardina/VideoActivityTest.java
+++ MyArdina/app/src/androidTest/java/com/myardina/buckeyes/myardina/VideoActivityTest.java
@@ -34,12 +34,14 @@ public class VideoActivityTest extends ActivityInstrumentationTestCase2<LoginAct
      */
     public void testVideoCallChoice() throws Exception {
         solo.unlockScreen();
+        //solo.sleep(5000);
         //This code just logs in and gets to symptom activity,
         //repeat of code of testing successful login from login activity test
         solo.waitForActivity(LoginActivity.class, 1000); // was 1000
         // check that we have the right activity
         solo.assertCurrentActivity("Expected Login activity", LoginActivity.class);
         //add username
+        solo.sleep(5000);
         EditText email = (EditText) solo.getCurrentActivity().findViewById(R.id.email);
         solo.enterText(email, "c@p.com");
         //add password
@@ -85,14 +87,29 @@ public class VideoActivityTest extends ActivityInstrumentationTestCase2<LoginAct
 
         //should be on payment activity now
         continueButton = (Button) solo.getCurrentActivity().findViewById(R.id.b_debug_to_doctors_available);
-        solo.waitForView(continueButton, 2000, false);
+        //solo.sleep(5000);
+        solo.waitForView(continueButton, 10000, false);
         solo.clickOnView(continueButton);
-
+        //solo.sleep(5000);
         //wait for and check that next activity is DoctorsAvailableActivity
-        solo.waitForActivity(DoctorsAvailableActivity.class, 6000); //was 2000
+        solo.waitForActivity(DoctorsAvailableActivity.class, 2000); //was 2000
         solo.assertCurrentActivity("Expected Doctors Available activity", DoctorsAvailableActivity.class);
-
         //Should be Available Doctors activity now, set doctor to available
+        solo.sleep(5000);
 
+        /*
+        To test:
+            -Get to call choice screen
+                -test : click on Video
+                 -test: click on Phone
+                 -Check that it gets to Video activity
+                 -Check that it gets to Phone activity
+                 -Check after choosing Video, back out to Choice screen and dialog buttons work
+                 -Check after choosing Phone, back out to Choice screen and dialog buttons work
+                 -Check after back out of video choice that back to choice activity
+                 -Check after back out of phone choice that back to choice activity
+
+
+         */
     }
 }
